14944f6b96ab6a58849440c12ebf3927ee87879a
Rebeca Bitner <rebecabitner@gmail.com>
2023-01-24 15:19:20 -0300

    Create ex_4.php

---
+++ exercicio_quatro/ex_4.php
@@ -0,0 +1,18 @@
+<?php
+
+$float_1 = 3.4;
+echo $float_1."<br>";
+$float_2 = 2.7;
+echo $float_2."<br>";
+$float_3 = 5.8;
+echo $float_3."<br>";
+
+
+if(is_float($float_3)){
+    echo "<br>É um float";
+
+}else{
+    echo "Não é um float";
+}
+
+?>
